8f83debadbad888d3199eb73153c435eac1ba7b9
Igor Santos <i.pereira.santos.is@gmail.com>
2023-01-31 20:23:46 -0300

    Frontend project with initial config created

---
+++ frontend/src/App.tsx
@@ -0,0 +1,20 @@
+import { ToastContainer } from "react-toastify";
+
+
+function App() {
+
+  return(
+    <>
+      <ToastContainer />
+      <main>
+        <section id="">
+          <div className="">
+            Hello World
+          </div>
+        </section>
+      </main>
+    </>
+  )
+}
+
+export default App;
